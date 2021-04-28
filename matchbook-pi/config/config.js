2e96b4dbdc63bdfbba4c29fefcf9e63f9adf1e45
DariDiniz <darianydiniz@gmail.com>
2021-04-28 13:43:45 -0300

    Add arquivos básicos

---
+++ matchbook-pi/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "username": "root",
+    "password": "Ventiladordemadeir@2021",
+    "database": "matchbook",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": "3306"
+  };
